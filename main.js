48450cbf7b99e8441967c06ad971352634228ad5
Lautaro Rojas <lautaroezequielrjs@gmail.com>
2023-10-16 16:14:27 -0300

    carrito

--- main.js
+++ main.js
@@ -1,66 +1,40 @@
-const precioPanBlanco=900
-const precioPanIntegral=1000
-
-function formadepago(pago,costo){
-    let precio
-    switch (pago){
-        case "Credito": precio = costo * 1.13
-        alert("El precio total es de: " + precio)
-        break
-        case "Debito":  precio = costo * 1.05
-        alert("El precio total es de: " + precio)
-        break
-        case "Efectivo": alert("El precio total es de: " + costo)
-        break
-        default: alert("Ha escrito mal la forma de pago, recuerde utilizar mayusculas donde corresponda")
-    }
-}
-
-let producto
-
-while(true){
-    producto = prompt("Eliga el producto que desea comprar: Pan Blanco, Pan Integral")
-    if(producto == "Pan Blanco" || producto == "Pan Integral"){
-        break;
+const productos = [
+    {nombre: "Pan Blanco", precio: 900},
+    {nombre: "Pan Integral", precio: 1000},
+    {nombre: "Prepizza", precio: 700},
+];
+let carrito = [];
+let todosLosProductos = productos.map((producto) => producto.nombre + " " + producto.precio + "$")
+alert("Bienvenido/a, te presentamos nuestros productos: " + todosLosProductos.join(" - "))
+    let producto = prompt("Agrega un producto a tu carrito de compras (Pan Blanco, Pan Integral, Prepizza)");
+    let precio=0;
+    if(producto == "Pan Blanco" || producto == "Pan Integral" || producto == "Prepizza"){
+        switch (producto){
+            case "Pan Blanco":
+                precio =n900;
+            break;
+            case "Pan Integral":
+                precio=1000;
+            break;
+            case "Pan Integral":
+                precio=700;
+            break;
+            default:
+            break;
+        }
+        alert(precio)
+        let cantidad = Number(prompt("Ingrese la cantidad que desee comprar"))
+        carrito.push({producto,cantidad,precio})
+        console.log(carrito);
     }
     else{
-        alert("Ha escrito mal el producto, recuerde utilizar mayusculas donde corresponda")
+            alert("Ha escrito mal el producto, recuerde utilizar mayusculas donde corresponda")
     }
+    let seleccion = prompt("Desea seguir comprando?")
+    while(seleccion === "No" || seleccion === "no"){
+        alert("Gracias por su compra, hasta pronto")
+        carrito.forEach((carritoFinal) => {
+        console.log(`Producto: ${carritoFinal.producto}, Unidades: ${carritoFinal.cantidad}, Total a pagar por producto ${carritoFinal.cantidad * carritoFinal.precio}`)
+        })
+        break;
 }
-
-let cantidad = Number(prompt("Ingrese la cantidad que desee comprar"))
-
-if(producto=="Pan Blanco"){
-    let costo = precioPanBlanco * cantidad 
-    let pago
-    while (true){
-        pago = prompt("Eliga la forma de pago: 'Credito'  13% recargo, 'Debito' 5% recargo, 'Efectivo' sin recargo")
-        if(pago == "Credito" || pago== "Debito" || pago== "Efectivo"){
-            break
-        }
-        else{
-            alert("Ha escrito mal la forma de pago, recuerde utilizar mayusculas donde corresponda")
-        }
-    }
-    formadepago(pago,costo)
-
-}
-else if(producto == "Pan Integral"){ 
-    let costo = precioPanIntegral * cantidad 
-    let pago
-    while(true){
-        pago = prompt("Eliga la forma de pago: 'Credito'  13% recargo, 'Debito' 5% recargo, 'Efectivo' sin recargo")
-        if(pago == "Credito" || pago== "Debito" || pago== "Efectivo"){
-            break
-        }
-        else{
-            alert("Ha escrito mal la forma de pago, recuerde utilizar mayusculas donde corresponda")
-        }
-    }
-    formadepago(pago,costo)
-}
-
-
-
-
-
