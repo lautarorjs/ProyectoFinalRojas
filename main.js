fbad2123b4c57fc3538c07f33e036eee7e015f15
Lautaro Rojas <lautaroezequielrjs@gmail.com>
2023-11-02 17:01:52 -0300

    carrito modal con nombre y precio

--- main.js
+++ main.js
@@ -1,57 +1,54 @@
 const productos = [
-    {nombre: "Pan Blanco", precio: 900},
-    {nombre: "Pan Integral", precio: 1000},
-    {nombre: "Pizza", precio: 3200},
+    { nombre: "Pan Blanco", precio: 900, img: "./images/pan-con-masa-madre-kBNB--1200x630@abc.jpg" },
+    { nombre: "Pan Integral", precio: 1000, img: "./images/pan_integral_de_masa_madre_700.jpg" },
+    { nombre: "Pizza", precio: 3200, img: "./images/pizza-masa-madre_5.jpg.webp" },
 ];
 
+let contenedor = document.getElementById("contenedor");
 let carrito = [];
 
-// let panBlanco = document.getElementsByClassName("panBlanco");
-// let panNegro = document.getElementsByClassName("panNegro");
-// let pizza = document.getElementsByClassName("pizza");
+productos.forEach((producto) => {
+    let card = document.createElement("div");
+    card.className = "card"
+    card.innerHTML = `
+        <img src="${producto.img}" alt="${producto.nombre}">
+        <p>${producto.nombre}</p>
+        <p>$${producto.precio}</p>
+        <button class="btn" onclick="agregarAlCarrito('${producto.nombre}','${producto.precio}')">Agregar al carrito</button>
+    `;
+    contenedor.appendChild(card);
+});
 
-// panBlanco.addEvenListen("click", agregarcarrito);
-// panNegro.addEvenListen("click", agregarcarrito);
-// pizza.addEvenListen("click", agregarcarrito);
-
-function agregarAlCarrito(producto){
-    let lista = document.getElementById("carrito");
-    let nuevoProducto = document.createElement('li');
-    nuevoProducto.innerText = producto 
-    lista.append(nuevoProducto);
+function agregarAlCarrito(nombre, precio) {
+    carrito.push({nombre:nombre,precio:precio});
+    console.log(carrito);
+    actualizarCarrito();
 }
 
-// while(true){
-//     let producto = prompt("Agrega un producto a tu carrito de compras (Pan Blanco, Pan Integral, Prepizza)");
-//     let precio=0;
-//     if(producto == "Pan Blanco" || producto == "Pan Integral" || producto == "Prepizza"){
-//         switch (producto){
-//             case "Pan Blanco":
-//                 precio =900;
-//             break;
-//             case "Pan Integral":
-//                 precio=1000;
-//             break;
-//             case "Prepizza":
-//                 precio=700;
-//             break;
-//             default:
-//             break;
-//         }
-//         alert(precio)
-//         let cantidad = Number(prompt("Ingrese la cantidad que desee comprar"))
-//         carrito.push({producto,cantidad,precio})
-//         console.log(carrito);
-//     }
-//     else{
-//         alert("Ha escrito mal el producto, recuerde utilizar mayusculas donde corresponda")
-//     }
-//     let seleccion = prompt("Desea seguir comprando?")
-//     while(seleccion === "No" || seleccion === "no"){
-//         alert("Gracias por su compra, hasta pronto")
-//         carrito.forEach((carritoFinal) => {
-//         console.log(`Producto: ${carritoFinal.producto}, Unidades: ${carritoFinal.cantidad}, Total a pagar por producto ${carritoFinal.cantidad * carritoFinal.precio}`)
-//         })
-//         break;
-//     }
-// }
+function actualizarCarrito() {
+    let lista = document.getElementById("carrito");
+    lista.innerHTML = '';
+
+    carrito.forEach(producto => {
+        let nuevoProducto = document.createElement('li');
+        nuevoProducto.innerText = `${producto.nombre} - $${producto.precio}`;
+        lista.appendChild(nuevoProducto);
+    });
+    
+    let cantidadCarrito = document.getElementById("cantidad-carrito");
+    cantidadCarrito.innerText = carrito.length;
+}
+
+
+function mostrarCarrito() {
+    let modal = document.getElementById("carrito-modal");
+    modal.style.display = "block";
+}
+
+function cerrarCarrito() {
+    // Cerrar el modal de carrito
+    let modal = document.getElementById("carrito-modal");
+    modal.style.display = "none";
+}
+
+
